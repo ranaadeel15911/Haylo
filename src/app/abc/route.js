ea0c6c24bda46f8a314ba68b2407cee07c5f3182
ranaadeel15911 <ra0511083@gmail.com>
2024-02-07 01:33:38 +0500

    afja;

---
+++ src/app/abc/route.js
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server"
+
+export const GET = (req)=>{
+const res =  NextResponse.json({
+    user:'adeel'
+})
+res.cookies.set('user', "adeel",{maxAge:30482304823048})
+res.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, private')
+return res
+// return NextResponse.json("GET")
+}
